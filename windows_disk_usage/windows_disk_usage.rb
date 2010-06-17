1cbfeb35486d74b464bb110122e03c53c2eeb796
Don Johnson <djohnsonjr@gmail.com>
2010-06-17 13:26:02 -0500

    adding windows disk stuff

---
+++ windows_disk_usage/windows_disk_usage.rb
@@ -0,0 +1,14 @@
+class WindowsDiskUsage < Scout::Plugin
+
+  def build_report
+    if `typeperf \"\\LogicalDisk(_Total)\\% Free Space\" -sc 1` =~ /,\"(\d*\.\d*)\"\n/
+      report :available => $1.to_f
+    else
+      raise "Couldn't use `typepref` as expected."
+    end
+  rescue Exception
+    error "Error determining load", $!.message
+  end
+
+end
+
