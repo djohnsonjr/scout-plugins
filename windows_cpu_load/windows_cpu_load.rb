315b4e3a1a2907fa56fc065ac41c324667d1e8e7
Don Johnson <djohnsonjr@gmail.com>
2010-06-17 13:36:48 -0500

    changing some labels

--- windows_cpu_load/windows_cpu_load.rb
+++ windows_cpu_load/windows_cpu_load.rb
@@ -2,7 +2,7 @@ class WindowsCPULoad < Scout::Plugin
 
   def build_report
     if `typeperf \"\\Processor(_Total)\\% Processor Time\" -sc 1` =~ /,\"(\d*\.\d*)\"\n/
-      report :usage => $1.to_f
+      report '% used' => $1.to_f
     else
       raise "Couldn't use `typepref` as expected."
     end
--- windows_disk_usage/windows_disk_usage.rb
+++ windows_disk_usage/windows_disk_usage.rb
@@ -2,7 +2,7 @@ class WindowsDiskUsage < Scout::Plugin
 
   def build_report
     if `typeperf \"\\LogicalDisk(_Total)\\% Free Space\" -sc 1` =~ /,\"(\d*\.\d*)\"\n/
-      report :available => $1.to_f
+      report '% available' => $1.to_f
     else
       raise "Couldn't use `typepref` as expected."
     end
