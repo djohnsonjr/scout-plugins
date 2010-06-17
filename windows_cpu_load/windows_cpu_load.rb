68027c3b73a467c2394681b9bef02997cfda84e4
Don Johnson <djohnsonjr@gmail.com>
2010-06-17 13:05:37 -0500

    Adding a point in time cpu stat for a windows box.

---
+++ windows_cpu_load/windows_cpu_load.rb
@@ -0,0 +1,14 @@
+class WindowsCPULoad < Scout::Plugin
+
+  def build_report
+    if `typeperf \"\\Memory\\Available KBytes\" -sc 1` =~ /,\"(\d*\.\d*)\"\n/
+      report :usage => $1.to_f
+    else
+      raise "Couldn't use `typepref` as expected."
+    end
+  rescue Exception
+    error "Error determining load", $!.message
+  end
+
+end
+
