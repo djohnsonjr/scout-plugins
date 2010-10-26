9c9a864d26c195538580b5a139823e47133ff552
unknown <djohnson@.peopleadmin.com>
2010-10-26 09:06:12 -0600

    Scanning all disks for size.

--- windows_disk_usage/windows_disk_usage.rb
+++ windows_disk_usage/windows_disk_usage.rb
@@ -1,8 +1,25 @@
 class WindowsDiskUsage < Scout::Plugin
 
   def build_report
-    if `typeperf \"\\LogicalDisk(_Total)\\% Free Space\" -sc 1` =~ /,\"(\d*\.\d*)\"\n/
-      report '% available' => $1.to_f
+    if results = `typeperf \"\\LogicalDisk(*)\\% Free Space\" -sc 1`
+			labels = []
+			data = []
+			puts results
+			lines = results.split("\n")
+			lines.each do |line|
+				columns = line.split(",")
+				columns.each do |column|
+					# puts "---#{column}---"
+					if column.match(/LogicalDisk\((\w:|_Total)\)/)
+						labels << $1
+					elsif column.match(/^\"(\d*\.\d*)\"$/)
+						data << $1
+					end
+				end
+			end
+			labels.each_with_index do |label, index|
+				report "#{label}% available" => data[index].to_f
+			end
     else
       raise "Couldn't use `typepref` as expected."
     end
