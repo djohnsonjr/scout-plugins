053739f2c40bb286139fd8f87886bba454832559
unknown <djohnson@.peopleadmin.com>
2010-10-26 15:45:38 -0600

    Adding a new file to check a given service.

---
+++ windows_service_check/windows_service_check.rb
@@ -0,0 +1,38 @@
+class WindowsServiceCheck < Scout::Plugin
+
+OPTIONS=<<-EOS
+service:
+	default: ~
+	name: Service
+	notes: The service to monitor
+EOS
+
+  def build_report
+		status = get_status
+		if status
+	    report :status => status
+		else
+			raise 'Error during process status'
+		end
+  rescue Exception
+    error "Error determining process status", $!.message
+  end
+
+	def get_status
+    if results = `typeperf \"\\Process(#{option(:service)})\\Working Set\" -sc 1`
+			# good ends with The command completed successfully
+			# not running ends with Error:\nthe data is not valid
+			if results.match(/completed successfully/)
+				"on"
+			else
+				"off"
+			end
+		else
+			raise 'Error during process status: Typeperf returned unexpected value.'
+		end
+	end
+
+end
+
+
+
